56a225fea77d7be892639d3ca2c6b06a33a8b158
Ajai <ajainarayanan@gmail.com>
2019-06-21 10:12:46 -0700

    s

--- cdap-ui/app/cdap/components/DataPrepConnections/PluginConnectionBrowser/PluginConnectionBrowserTypes.ts
+++ cdap-ui/app/cdap/components/DataPrepConnections/PluginConnectionBrowser/PluginConnectionBrowserTypes.ts
@@ -22,7 +22,15 @@ interface IPluginNode {
   plugin: IPlugin;
   type: string;
 }
-
+/**
+ * TODO: We might need this in the future where we specify the pipeline
+ * type as well in widget attributes. Right now only kafka allows to add
+ * source and wrangler as part of both batch and realtime pipelines.
+ *
+ * When tomorrow we have connections where the source plugin has different
+ * properties between batch and realtime pipeline then we need to generate the right
+ * properties to autofill.
+ */
 interface IWidgetAttributes {
   connectionType: ConnectionType;
 }
