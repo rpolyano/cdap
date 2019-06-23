cde983f27b6ced31b01ec720f501a1005ea6196d
Ajai <ajainarayanan@gmail.com>
2019-06-23 11:17:41 -0700

    Adds e2e tests for GCS, Spanner and BigQuery browsers for dataprep

--- cdap-ui/cypress/support/commands.ts
+++ cdap-ui/cypress/support/commands.ts
@@ -14,6 +14,8 @@
  * the License.
 */
 
+import { ConnectionType } from '../../app/cdap/components/DataPrepConnections/ConnectionType';
+import { DEFAULT_GCP_PROJECTID, DEFAULT_GCP_SERVICEACCOUNT_PATH } from '../support/constants';
 /**
  * Uploads a pipeline json from fixtures to input file element.
  *
@@ -56,4 +58,100 @@ Cypress.Commands.add('cleanup_pipelines', (headers, pipelineName) => {
         });
       }
     });
+});
+
+Cypress.Commands.add(
+  'fill_GCS_connection_create_form',
+  (
+    connectionId,
+    projectId = DEFAULT_GCP_PROJECTID,
+    serviceAccountPath = DEFAULT_GCP_SERVICEACCOUNT_PATH
+  ) => {
+    cy.visit('/cdap/ns/default/connections');
+    cy.get('[data-cy="wrangler-add-connection-button"]').click();
+    cy.get(`[data-cy="wrangler-connection-${ConnectionType.GCS}`).click();
+    cy.get(`[data-cy="wrangler-${ConnectionType.GCS}-connection-name"]`).type(connectionId);
+    cy.get(`[data-cy="wrangler-${ConnectionType.GCS}-connection-projectid"]`).type(projectId);
+    cy.get(`[data-cy="wrangler-${ConnectionType.GCS}-connection-serviceaccount-filepath"]`).type(
+      serviceAccountPath
+    );
+  }
+);
+
+Cypress.Commands.add('create_GCS_connection', (connectionId) => {
+  cy.fill_GCS_connection_create_form(connectionId);
+  cy.get(`[data-cy="wrangler-${ConnectionType.GCS}-add-connection-button"]`).click({
+    timeout: 60000,
+  });
+});
+
+Cypress.Commands.add('test_GCS_connection', (connectionId, projectId, serviceAccountPath) => {
+  cy.fill_GCS_connection_create_form(connectionId, projectId, serviceAccountPath);
+  cy.get(`[data-cy="wrangler-${ConnectionType.GCS}-test-connection-button"]`).click({
+    timeout: 60000,
+  });
+});
+
+Cypress.Commands.add(
+  'fill_BIGQUERY_connection_create_form',
+  (
+    connectionId,
+    projectId = DEFAULT_GCP_PROJECTID,
+    serviceAccountPath = DEFAULT_GCP_SERVICEACCOUNT_PATH
+  ) => {
+    cy.visit('/cdap/ns/default/connections');
+    cy.get('[data-cy="wrangler-add-connection-button"]').click();
+    cy.get(`[data-cy="wrangler-connection-${ConnectionType.BIGQUERY}`).click();
+    cy.get(`[data-cy="wrangler-${ConnectionType.BIGQUERY}-connection-name"]`).type(connectionId);
+    cy.get(`[data-cy="wrangler-${ConnectionType.BIGQUERY}-connection-projectid"]`).type(projectId);
+    cy.get(
+      `[data-cy="wrangler-${ConnectionType.BIGQUERY}-connection-serviceaccount-filepath"]`
+    ).type(serviceAccountPath);
+  }
+);
+
+Cypress.Commands.add('create_BIGQUERY_connection', (connectionId) => {
+  cy.fill_BIGQUERY_connection_create_form(connectionId);
+  cy.get(`[data-cy="wrangler-${ConnectionType.BIGQUERY}-add-connection-button"]`).click({
+    timeout: 60000,
+  });
+});
+
+Cypress.Commands.add('test_BIGQUERY_connection', (connectionId, projectId, serviceAccountPath) => {
+  cy.fill_BIGQUERY_connection_create_form(connectionId, projectId, serviceAccountPath);
+  cy.get(`[data-cy="wrangler-${ConnectionType.BIGQUERY}-test-connection-button"]`).click({
+    timeout: 60000,
+  });
+});
+
+Cypress.Commands.add(
+  'fill_SPANNER_connection_create_form',
+  (
+    connectionId,
+    projectId = DEFAULT_GCP_PROJECTID,
+    serviceAccountPath = DEFAULT_GCP_SERVICEACCOUNT_PATH
+  ) => {
+    cy.visit('/cdap/ns/default/connections');
+    cy.get('[data-cy="wrangler-add-connection-button"]').click();
+    cy.get(`[data-cy="wrangler-connection-${ConnectionType.SPANNER}`).click();
+    cy.get(`[data-cy="wrangler-${ConnectionType.SPANNER}-connection-name"]`).type(connectionId);
+    cy.get(`[data-cy="wrangler-${ConnectionType.SPANNER}-connection-projectid"]`).type(projectId);
+    cy.get(
+      `[data-cy="wrangler-${ConnectionType.SPANNER}-connection-serviceaccount-filepath"]`
+    ).type(serviceAccountPath);
+  }
+);
+
+Cypress.Commands.add('create_SPANNER_connection', (connectionId) => {
+  cy.fill_SPANNER_connection_create_form(connectionId);
+  cy.get(`[data-cy="wrangler-${ConnectionType.SPANNER}-add-connection-button"]`).click({
+    timeout: 60000,
+  });
+});
+
+Cypress.Commands.add('test_SPANNER_connection', (connectionId, projectId, serviceAccountPath) => {
+  cy.fill_SPANNER_connection_create_form(connectionId, projectId, serviceAccountPath);
+  cy.get(`[data-cy="wrangler-${ConnectionType.SPANNER}-test-connection-button"]`).click({
+    timeout: 60000,
+  });
 });
